a0eff6b9858b4a06d39fe7f9cba381d5f03731af
Adarsh <44157141+adarsh115@users.noreply.github.com>
2020-12-17 18:19:23 +0530

    empty commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
-import './App.css';
+import './app.css';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="app">
+      <h1>hel</h1>
     </div>
   );
 } 
 
 export default App;
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyAJNKQBtNWxpd2dePQnW0QAf5q9b9MXpnQ",
+//   authDomain: "mydiscord-8afb5.firebaseapp.com",
+//   projectId: "mydiscord-8afb5",
+//   storageBucket: "mydiscord-8afb5.appspot.com",
+//   messagingSenderId: "405621321398",
+//   appId: "1:405621321398:web:547bb5f2193d5957b70017",
+//   measurementId: "G-7KHR3T2KWB"
+// };
